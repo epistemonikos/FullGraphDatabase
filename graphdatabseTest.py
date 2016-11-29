fc225a5f6daf21a81d859871e13f2c9a363c9084
Fabian Mosso Chavez <fmosso@klabs.cl>
2016-11-29 11:22:30 -0300

    This is where it all begins...

--- graphdatabseTest.py
+++ graphdatabseTest.py
@@ -7,31 +7,37 @@ client = pyorient.OrientDB("localhost", 2424)  #TO DO
 session_id = client.connect( "admin", "admin" )
 
 #create a databse
-#client.db_create( "test1", pyorient.DB_TYPE_GRAPH, pyorient.STORAGE_TYPE_MEMORY )
+client.db_create( "test3", pyorient.DB_TYPE_GRAPH, pyorient.STORAGE_TYPE_MEMORY )
 
 
-client.db_open( "test1", "admin", "admin" )
+client.db_open( "test3", "admin", "admin" )
 
-#client.command( "create class Paper extends V" )
-#client.command( "create class Reference extends E" )
+client.command( "create class Paper extends V" )
+client.command( "create class Reference extends E" )
 
 
-#client.command( "INSERT INTO Paper CONTENT {name: \"paper1\"}")
-#client.command( "INSERT INTO Paper CONTENT {name: \"paper2\"}")
-#client.command( "INSERT INTO Paper CONTENT {name: \"paper3\"}")
+def logInsert(JSON):
+    file = open("log.txt", "a")
+    file.write("Inserted Doc, with doi " + JSON.get("ids").get("doi")+"\n" )
+    file.close()
 
-#client.command("create edge Reference from (select from Paper where name = 'paper1') to (select from Paper where name = 'paper2')")
+def logReference(id1,id2):
+    file = open("log.txt", "a")
+    file.write("Make reference between doc1 with doi "+id1+" and doc2 with doi "+ id2+"\n"  )
+    file.close()
 
-count = 1
+
 def insertPaper(JSON):
+    logInsert(json.loads(JSON))
     try:
-        client.command( "INSERT INTO Paper CONTENT" + str(JSON))
+         client.command( "INSERT INTO Paper CONTENT" + str(JSON))
     except:
         pass
 
-def makeReference(pbid1,pbid2):
+def makeReference(id1,id2):
+    logReference(id1,id2)
     try:
-        client.command("create edge Reference from (select from Paper where ids.doi = \""+pbid2+"\") to (select from Paper where ids.doi =\""+pbid1+"\")")
+        client.command("create edge Reference from (select from Paper where ids.doi = \""+id2+"\") to (select from Paper where ids.doi =\""+id1+"\")")
     except:
         pass
 
@@ -47,7 +53,7 @@ def readTSV(file_path):
         info["publication_info"] = linejson["publication_info"]
         info["keywords"] = linejson["keywords"]
         info["citation"] = linejson["citation"]
-        return json.loads(info)
+        return json.dumps(info)
     def getReferences(linejson):
         return linejson['references']
     while line:
@@ -58,7 +64,8 @@ def readTSV(file_path):
        for r in references:
             r = json.dumps(r)
             insertPaper(r)
-            print(rs)
+            rs = json.loads(rs)
+            r = json.loads(r)
             makeReference(rs.get("ids").get("doi"), r.get("ids").get("doi"))
        line = f.readline()
 
