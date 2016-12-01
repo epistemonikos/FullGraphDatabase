59ba68d05beed139756bb040bea7061083a3d932
Sam Hernandez <sam.hv@icloud.com>
2016-12-01 10:02:48 -0300

    added: resources

--- logs/graph_log.py
+++ logs/graph_log.py
@@ -1,9 +1,11 @@
 from logs.log import Log
 from model.graph import Graph
 
+LOG_FILE_PATH = "resources/log.log"
+
 @Log(Graph, Graph.insert)
 def log_insert(self, node, returned):
-    file = open("log.log", "a")
+    file = open(LOG_FILE_PATH, "a")
     if returned:
       inserted = "Inserted"
     else:
@@ -15,7 +17,7 @@ def log_insert(self, node, returned):
 def log_make_reference(self, node_1, node_2, returned):
     id1 = node_1.get_doi()
     id2 = node_2.get_doi()
-    file = open("log.log", "a")
+    file = open(LOG_FILE_PATH, "a")
     if returned:
       maked = "Maked"
     else:
